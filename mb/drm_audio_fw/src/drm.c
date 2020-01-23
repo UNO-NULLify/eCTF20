def5c8fc372b522e4f02c231f479c9fedffcd7c8
ArtificialAmateur <20297606+ArtificialAmateur@users.noreply.github.com>
2020-01-23 15:38:13 -0600

    Add login delay

--- mb/drm_audio_fw/src/drm.c
+++ mb/drm_audio_fw/src/drm.c
@@ -1,4 +1,5 @@
 #include "drm.h"
+
 #include "include/sodium.h"
 
 //////////////////////// GLOBALS ////////////////////////
@@ -16,68 +17,75 @@ drm_md d;
  * 1 = logged in
  * 0 = logged out
  */
-int GetLogin() {
-    return u.logged_in;
-}
+int GetLogin() { return u.logged_in; }
 
 // Returns number of provisioned users
-int GetUserTotal() {
-    return d.num_user;
-}
+int GetUserTotal() { return d.num_user; }
 
 //////////////////////// COMMAND FUNCTIONS ////////////////////////
 void Login(char[] username, char[] pin) {
-    // check if logged in
-    if (GetLogin()) {
-        mb_printf("User already logged-in.\r\n");
-    } else {
-        // search username
-        int i;
-        for (i = 0; i <= GetUserTotal(); i++) {
-            if (sodium_memcmp(creds[i][0], username)) {
-                // generate and search hash
-                if (crypto_pwhash_str_verify
-                            (creds[i][1], pin, strlen(pin))) {
-                    u.user = creds[i][0];
-                    u.hash = creds[i][1];
-                    u.logged_in = 1;
-                }
-                mb_printf("User not found\r\n");
-                sodium_memzero(u, sizeof(u));
-            }
+  // check if logged in
+  if (GetLogin()) {
+    mb_printf("User already logged-in.\r\n");
+  } else {
+    // search username
+    int i;
+    for (i = 0; i <= GetUserTotal(); i++) {
+      if (sodium_memcmp(creds[i][0], username)) {
+        // generate and search hash
+        if (crypto_pwhash_str_verify(creds[i][1], pin, strlen(pin))) {
+          u.user = creds[i][0];
+          u.hash = creds[i][1];
+          u.logged_in = 1;
         }
-    }
-}
-void LogOut() {
-    // check if logged in
-    if (GetLogin()) {
-        mb_printf("Logging out...\r\n");
-        // zero-out user struct
+        mb_printf("User not found\r\n");
         sodium_memzero(u, sizeof(u));
-        sodium_mem
+        // delay failed attempt by 5 seconds
+        sleep(LOGIN_DELAY);
+      }
+    }
+  }
+}
+
+void LogOut() {
+  // check if logged in
+  if (GetLogin()) {
+    mb_printf("Logging out...\r\n");
+    // zero-out user struct
+    sodium_memzero(u, sizeof(u));
+    sodium_mem
         // double check?
         u.logged_in = 0;
-    } else {
-        mb_printf("Not logged in\r\n");
-    }
+  } else {
+    mb_printf("Not logged in\r\n");
+  }
 }
-void Share(){
-    // check if logged in
-}
-void Query(){
-    // check if logged in
-}
-void DigitalOut(){
-    // check if logged in
-}
-void Play() {
-    // check if logged in
-    // check if song is playing
-    // check who is logged in and whether they have access
 
-    /* pause
-     * resume
-     * stop
-     * restart
-     */
+void Share() {
+  // check if logged in
+}
+
+void Query() {
+  // check if logged in
+}
+
+void DigitalOut() {
+  // check if logged in
+}
+
+void Play() {
+  // check if logged in
+  // check if song is playing
+  // check who is logged in and whether they have access
+
+  /* pause
+   * resume
+   * stop
+   * restart
+   */
+}
+
+int main() {
+
+
 }
--- mb/drm_audio_fw/src/drm.h
+++ mb/drm_audio_fw/src/drm.h
@@ -5,28 +5,29 @@
 #ifndef DRM_AUDIO_FW_DRM_H
 #define DRM_AUDIO_FW_DRM_H
 
+#define LOGIN_DELAY 5 // microseconds
+
 #define MAX_USERNAME_SZ 64
 #define MAX_HASH_SZ crypto_pwhash_STRBYTES
 #define MAX_USERS 64
 
-
 typedef struct {
-    char user[MAX_USERNAME_SZ];
-    char hash[MAX_HASH_SZ];
-    int logged_in;
+  char user[MAX_USERNAME_SZ];
+  char hash[MAX_HASH_SZ];
+  int logged_in;
 } user_md;
 
 typedef struct {
-    char owner[MAX_USERNAME_SZ];
-    char shared[MAX_USERS][MAX_USERNAME_SZ];
-    char regions[MAX_REGIONS][REGION_NAME_SZ];
-    
+  char owner[MAX_USERNAME_SZ];
+  char shared[MAX_USERS][MAX_USERNAME_SZ];
+  char regions[MAX_REGIONS][REGION_NAME_SZ];
+
 } song_md;
 
 typedef struct {
-    int num_user;
-    int num_region;
+  int num_user;
+  int num_region;
 
 } drm_md;
 
-#endif //DRM_AUDIO_FW_DRM_H
+#endif  // DRM_AUDIO_FW_DRM_H
