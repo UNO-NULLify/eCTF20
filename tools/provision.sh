0f2f3eb82c8867c37935da04d614446a4d47cea2
Jeffallan <dshostakovich30@gmail.com>
2020-01-26 17:08:03 -0600

    Beginning feature: python encryption.  Added build script (provision.sh) and Makefile for testing.  Note pvt key has an unencrypted field for debuging purposes.  Added pin, pin hash, salt, private key, public key, hardware secret in createUsers.  TODO in createUsers: add encryption for private key, add root private and public key.  Add public key, private key, and random pass to createRegions.  TODO in createRegions private key encryption.

---
+++ tools/provision.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#
+# A bash script to test the front end encription process see also Makefile
+#
+# Notes:
+#
+# adds metadata after the 44 byte header
+# metadata is b'\x06\x00\x01\x00\x00\x00'  little endian 0600 0100 0000
+# RIFF (.wav) header is 44 bytes long
+
+python3 createRegions --region-list USA Canada Japan --outfile region_secrets.json
+
+python3 createUsers --user-list user1:12345678 user2:12345679 user3:12345677 --outfile user_secrets.json
+
+#python3 createDevice --region-list Canada --region-secrets-path region_secrets.json --user-list user1 user2 --user-secrets-path user_secrets.json --device-dir ./device
+
+#python3 protectSong --region-list USA --region-secrets-path region_secrets.json --outfile test-protect.wav --infile Sound-Bite_One-Small-Step.wav --owner user1 --user-secrets-path user_secrets.json
