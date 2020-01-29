aa97a8af71fae5eb702ae0de335e3e4a07b7fcb7
Jeffallan <dshostakovich30@gmail.com>
2020-01-29 14:48:15 -0600

    remove root signature and rename pin to pin hast.

--- tools/test.c
+++ tools/test.c
@@ -7,5 +7,5 @@ int main(void)
     printf("Rand: %s\nPub: %s\nPriv: %s\n\n", Canada.rand, Canada.pub_key,Canada.priv_key);
 
     puts("Users:");
-    printf("HW: %s\nPin: %s\nPub: %s\nPriv: %s\n", user1.hw_secret, user1.pin, user1.pub_key, user1.pvt_key);
+    printf("HW: %s\nPin: %s\nPub: %s\nPriv: %s\n", user1.hw_secret, user1.pin_hash, user1.pub_key, user1.pvt_key);
 }
