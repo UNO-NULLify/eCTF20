d5a018449f2bc0b6f6cc1efefbbbd7813024cca4
Artificial <20297606+ArtificialAmateur@users.noreply.github.com>
2020-02-01 21:14:08 +0000

    Im sorry

--- vagrant/customizations.sh
+++ vagrant/customizations.sh
@@ -1,5 +1,6 @@
 #add tools to the path
 #THIS MUST BE UPDATED TO WORK WITH YOUR DESIGN SO THAT ALL TOOLS ARE ON THE PATH
-export PATH = "/ectf/tools:$PATH" echo "export PATH=/ectf/tools:$PATH" >>
-              ~ /.bashrc sudo apt install - y python3 - pip clang -
-                  format sudo pip3 install numpy
+export PATH = "/ectf/tools:$PATH" 
+echo "export PATH=/ectf/tools:$PATH" >> ~/.bashrc 
+sudo apt install -y python3-pip clang-format 
+sudo pip3 install numpy
