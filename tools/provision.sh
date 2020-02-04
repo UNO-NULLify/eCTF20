93b0817f2dd871186527bb1f27716f9791ea48b3
Andrew Griess <agriess@unomaha.edu>
2020-02-04 10:43:57 -0800

    added encryption keys - we just need to encrypt the files now

--- tools/provision.sh
+++ tools/provision.sh
@@ -12,10 +12,10 @@ python3 createRegions --region-list USA Canada Japan --outfile region_secrets.js
 
 python3 createUsers --user-list user1:12345678 user2:12345679 user3:12345677 --outfile user_secrets.json
 
-python3 createDevice --region-list Canada USA --region-secrets-path region_secrets.json --user-list user1 user2 --user-secrets-path user_secrets.json --device-dir ./device
+#python3 createDevice --region-list Canada USA --region-secrets-path region_secrets.json --user-list user1 user2 --user-secrets-path user_secrets.json --device-dir ./device
 
-gcc -Wall -pedantic -std=c1x -g -o  ./device/test test.c -lsodium
+#gcc -Wall -pedantic -std=c1x -g -o  ./device/test test.c -lsodium
 
-./device/test
+#./device/test
 
-#python3 protectSong --region-list USA --region-secrets-path region_secrets.json --outfile test-protect.wav --infile Sound-Bite_One-Small-Step.wav --owner user1 --user-secrets-path user_secrets.json
+python3 protectSong --region-list USA Canada --region-secrets-path region_secrets.json --outfile test-protect.wav --infile Sound-Bite_One-Small-Step.wav --owner user1 --user-secrets-path user_secrets.json
