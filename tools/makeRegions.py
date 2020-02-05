0838feb169f2086331ef96a35d58737c30b3159a
Jeffallan <dshostakovich30@gmail.com>
2020-02-04 20:25:21 -0600

    add regions for testing and update customize.sh to include pip install pydub

--- tools/makeRegions.py
+++ tools/makeRegions.py
@@ -1,1 +1,41 @@
-# A testing script that generates ___ random regions
+# A testing script that generates 32 regions
+
+countries = "Afghanistan \
+Albania \
+Algeria \
+Andorra \
+Angola \
+Anguilla \
+Argentina \
+Armenia \
+Aruba \
+Australia \
+Austria \
+Azerbaijan \
+Bahamas \
+Bahrain \
+Bangladesh \
+Barbados \
+Belarus \
+Belgium \
+Belize \
+Benin \
+Bermuda \
+Bhutan \
+Bolivia \
+Botswana \
+Brazil \
+Bulgaria \
+Burundi \
+Japan \
+Canada \
+Chile \
+USA \
+Colombia"
+
+if __name__ == "__main__":
+    
+    print("Generating Test Regions")
+    print(countries)
+    file = open(os.getcwd() + "/test_regions.txt","w")
+    file.writelines(countries)
