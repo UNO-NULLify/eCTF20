460afce463e4bf6b691aa8da42d1fa3494c0a0da
Jeffallan <dshostakovich30@gmail.com>
2020-02-04 18:07:16 -0600

    generate 128 mb test audio add feature make_sample() which creates a 30 second sample from a .wav file

---
+++ tools/makeAudio.py
@@ -0,0 +1,38 @@
+# A sample script that generates a constant tone for test purposes
+
+from pydub.generators import Sine
+from pydub import AudioSegment
+from pydub.playback import play
+import os
+
+
+SAMPLE_RATE = 44100
+BIT_DEPTH = 16
+DURATION = 726 * 1000  # duration is in milliseconds
+
+"""
+a function that makes a sample of a provisioned song
+
+param: file_path: The file path to the provisioned song
+param mil_sec: length of sample in milliseconds default 30,000 (30 sec) 
+"""
+def make_sample(file_path, milsec=30000):
+    sample = AudioSegment.from_file(file_path, format="wav")
+    f_name = file_path.split(".")[0]
+    sample[0:milsec].export(f_name + "_sample.wav", format="wav")
+
+if __name__ == "__main__":
+
+    print("\nGenerating Test Audio")
+    sound1 = Sine(440, sample_rate=SAMPLE_RATE, bit_depth=BIT_DEPTH)   # constant tone at 440hz
+    sample1 = sound1.to_audio_segment(duration=DURATION)
+    sound2 = Sine(440, sample_rate=SAMPLE_RATE, bit_depth=BIT_DEPTH)   # constant tone at 440hz
+    sample2 = sound2.to_audio_segment(duration=DURATION)
+    mix = AudioSegment.from_mono_audiosegments(sample1, sample2)
+   
+    mix.export(os.getcwd() + "/device/test.wav", format="wav")
+    print("Generating Sample")
+    make_sample(os.getcwd() + "/device/test.wav")
+    #mix[0:30000].export(os.getcwd() + "/device/30_test.wav", format="wav")
+
+    #play(sample)
--- tools/provision.sh
+++ tools/provision.sh
@@ -8,14 +8,29 @@
 # metadata is b'\x06\x00\x01\x00\x00\x00'  little endian 0600 0100 0000
 # RIFF (.wav) header is 44 bytes long
 
+# generate test data
+
+# makeUsers.py
+# makeRegions.py
+
+# end generate test data 
+
 python3 createRegions --region-list USA Canada Japan --outfile region_secrets.json
 
 python3 createUsers --user-list user1:12345678 user2:12345679 user3:12345677 --outfile user_secrets.json
 
-#python3 createDevice --region-list Canada USA --region-secrets-path region_secrets.json --user-list user1 user2 --user-secrets-path user_secrets.json --device-dir ./device
+python3 createDevice --region-list Canada USA --region-secrets-path region_secrets.json --user-list user1 user2 --user-secrets-path user_secrets.json --device-dir ./device
 
-#gcc -Wall -pedantic -std=c1x -g -o  ./device/test test.c -lsodium
+# Generate Test Song
 
-#./device/test
+python3 makeAudio.py
+
+# End Generate Test Song
 
 python3 protectSong --region-list USA Canada --region-secrets-path region_secrets.json --outfile test-protect.wav --infile Sound-Bite_One-Small-Step.wav --owner user1 --user-secrets-path user_secrets.json
+
+# Time to Reverse the process
+
+gcc -Wall -pedantic -std=c1x -g -o  ./device/test test.c -lsodium
+
+./device/test
