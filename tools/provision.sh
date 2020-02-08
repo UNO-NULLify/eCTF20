d6d9ea1766e7a18ce9aa722680f844386b672b6d
Andrew Griess <agriess@unomaha.edu>
2020-02-07 21:12:25 -0800

    encrypting song and 30 song with encryption keys

--- tools/provision.sh
+++ tools/provision.sh
@@ -10,26 +10,38 @@
 
 # generate test data
 
-python3 makeUsers.py
+#python3 makeUsers.py
 # python3 makeRegions.py
 
-# end generate test data 
+# end generate test data
 
 python3 createRegions --region-list USA Canada Japan --outfile region_secrets.json
 
-#python3 createUsers --user-list user1:12345678 user2:12345679 user3:12345677 --outfile user_secrets.json
+python3 createUsers --user-list user1:12345678 user2:12345679 user3:12345677 --outfile user_secrets.json
 
-python3 createUsers --user-list $(cat test_users.txt) --outfile user_secrets.json
+#python3 createUsers --user-list $(cat test_users.txt) --outfile user_secrets.json
 
-python3 createDevice --region-list Canada USA --region-secrets-path region_secrets.json --user-list $(cat test_users.txt | awk '{print $1 " " $2}' | sed 's/:/ /g' | awk '{print $1 " " $3}') --user-secrets-path user_secrets.json --device-dir ./device
+#python3 createDevice --region-list Canada USA --region-secrets-path region_secrets.json --user-list $(cat test_users.txt | awk '{print $1 " " $2}' | sed 's/:/ /g' | awk '{print $1 " " $3}') --user-secrets-path user_secrets.json --device-dir ./device
 
 # Generate Test Song
 
-python3 makeAudio.py
+#python3 makeAudio.py
 
 # End Generate Test Song
 
-python3 protectSong --region-list USA Canada --region-secrets-path region_secrets.json --outfile test-protect.wav --infile Sound-Bite_One-Small-Step.wav --owner $(cat test_users.txt | awk '{print $1}' | sed 's/:/ /g' | awk '{print $1}') --user-secrets-path user_secrets.json
+
+gcc -Wall -pedantic -std=c1x -g -o  ./encryptFile encryptFile.c -lsodium
+gcc -Wall -pedantic -std=c1x -g -o  ./decryptFile decryptFile.c -lsodium
+
+
+python3 newProtectSong --region-list USA Canada --region-secrets-path region_secrets.json --outfile test-protect.wav --infile ../sample-audio/Sound-Bite_One-Small-Step.wav --owner user1 --user-secrets-path user_secrets.json
+
+
+
+
+#python3 protectSong --region-list USA Canada --region-secrets-path region_secrets.json --outfile test-protect.wav --infile Sound-Bite_One-Small-Step.wav --owner $(cat test_users.txt | awk '{print $1}' | sed 's/:/ /g' | awk '{print $1}') --user-secrets-path user_secrets.json
+
+
 
 # Time to Reverse the process
 
