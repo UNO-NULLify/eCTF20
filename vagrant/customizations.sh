fcce350230eebef8060ce8c54f8e4743e78cffc0
vagrant <vagrant@vagrant-eCTF>
2020-02-10 18:12:29 +0000

    add install for libsodium (c version).

--- vagrant/customizations.sh
+++ vagrant/customizations.sh
@@ -5,3 +5,9 @@ echo "export PATH=/ectf/tools:$PATH" >> ~/.bashrc
 sudo apt install -y python3-pip clang-format
 sudo pip3 install numpy PyNaCl pydub
 
+# install LibSodium
+# wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable.tar.gz
+# tar -xf libsodium-1.0.18-stable.tar.gz
+# ./configure/libsodium-stable
+# make && make check && make install
+
