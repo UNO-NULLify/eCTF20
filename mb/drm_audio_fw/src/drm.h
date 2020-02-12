42095fd1ef672f587b779e555d18b2f7eaa64dbf
ArtificialAmateur <20297606+ArtificialAmateur@users.noreply.github.com>
2020-02-12 14:39:24 -0600

    Add song metadata struct

--- mb/drm_audio_fw/src/drm.h
+++ mb/drm_audio_fw/src/drm.h
@@ -1,11 +1,17 @@
 #ifndef DRM_AUDIO_FW_DRM_H
 #define DRM_AUDIO_FW_DRM_H
 
+#include "xil_printf.h"
+#include "secrets.h"
+
 #define LOGIN_DELAY 5 // seconds
 
-#define MAX_USERNAME_SZ 64
+#define MAX_USERNAME_NAME 16
 #define MAX_HASH_SZ crypto_pwhash_STRBYTES
 #define MAX_USERS 64
+#define MAX_SONG_NAME
+#define MAX_REGION_NAME 64
+#define MAX_REGION_SECRET 160
 
 typedef enum states { STOPPED, WORKING, PLAYING, PAUSED } STATE;
 
@@ -19,9 +25,11 @@ typedef struct {
 } user_md;
 
 typedef struct {
-  char owner[MAX_USERNAME_SZ];
-  char shared[MAX_USERS][MAX_USERNAME_SZ];
-  char regions[MAX_REGIONS][REGION_NAME_SZ];
+  char owner[MAX_USERNAME_NAME];
+  char shared[PROVISIONED_USERS][MAX_USERNAME_SZ];
+  char song_name[MAX_SONG_NAME]; //null terminated string of size 15 or less
+  char region_list[PROVISIONED_REGIONS][MAX_REGION_NAME]; // 32 regions max of size 64 len + null
+  char region_secret_list[PROVISIONED_REGIONS][MAX_REGION_SECRET];  // 32 regions max of size 160 + null
 
 } song_md;
 
