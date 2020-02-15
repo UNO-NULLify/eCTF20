7c55b49c9bd7053f31b528a76f456fb5eaf68753
ArtificialAmateur <20297606+ArtificialAmateur@users.noreply.github.com>
2020-02-15 14:58:34 -0600

    Modify output path for download.bit

--- tools/combine_bitstream.tcl
+++ tools/combine_bitstream.tcl
@@ -12,12 +12,13 @@ if { $argc != 1 } {
 }
 
 set dev_path [lindex $argv 0]
+set proj_name [lindex $argv 1]
 set worksp "$dev_path/mb"
 
 set sw_mmi "$worksp/Cora-Z7-07S/system_wrapper.mmi"
 set sw_bit "$worksp/Cora-Z7-07S/system_wrapper.bit"
 set drm_elf "$worksp/drm_audio_fw/Debug/drm_audio_fw.elf"
-set output "$worksp/Cora-Z7-07S/download.bit"
+set output "$dev_path/pl/proj/$proj_name/download.bit"
 
 puts "\nCalling updatemem as follows:
 updatemem -force -meminfo \
