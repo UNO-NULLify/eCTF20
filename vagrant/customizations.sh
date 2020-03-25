e18b876d4e51158fa04cc1851f1b304f760fc034
Aaron Wurtele <aaronwurtele@gmail.com>
2020-03-25 17:51:15 -0500

    removing all of the old stuff from customizations.sh

--- vagrant/customizations.sh
+++ vagrant/customizations.sh
@@ -3,11 +3,7 @@
 export PATH="/ectf/tools:$PATH"
 echo "export PATH=/ectf/tools:$PATH" >> ~/.bashrc
 sudo apt install -y python3-pip clang-format
-sudo pip3 install numpy PyNaCl pydub
+sudo pip3 install numpy pydub
 
-# install LibSodium
-# wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable.tar.gz
-# tar -xf libsodium-1.0.18-stable.tar.gz
-# ./configure/libsodium-stable
 # make && make check && make install
 
